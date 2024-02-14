424025751214ce91c761b20c3926c00e4ecd5b7d
Ahnaf Hassan <levi6516@gmail.com>
2024-02-15 02:20:52 +0600

    login and signup backend

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
+const validator = require('validator')
 
 const Schema = mongoose.Schema
 
@@ -19,6 +20,17 @@ const userSchema = new Schema({
 
 userSchema.statics.signup = async function(email, password) {
 
+    // validation
+    if (!email || !password) {
+      throw Error('All fields must be filled')
+    }
+    if (!validator.isEmail(email)) {
+      throw Error('Email not valid')
+    }
+    if (!validator.isStrongPassword(password)) {
+      throw Error('Password not strong enough')
+    }
+  
     const exists = await this.findOne({ email })
   
     if (exists) {
@@ -33,5 +45,28 @@ userSchema.statics.signup = async function(email, password) {
     return user
   }
   
+
+  // login method
+
+userSchema.statics.login = async function(email, password) {
+
+    if (!email || !password) {
+        throw Error('All fields must be filled')
+      }
+
+    const user = await this.findOne({ email })
+
+    if (!user) {
+    throw Error('Incorrect email')
+    }
+
+    const match = await bcrypt.compare(password, user.password)
+
+    if (!match){
+        throw Error('Incorrect password')
+    }
+
+    return user
+}
 
 module.exports = mongoose.model('User', userSchema)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import { Navbar } from './Components/Navbar/Navbar';
 import {Home} from './Components/Home/Home';
+import Login from './Pages/Login'
+import Signup from './Pages/Signup'
 import {Routes,Route, useLocation } from 'react-router-dom';
 import {Footer} from './Components/Footer/Footer';
 import {PostContent} from './Components/PostContent/PostContent';
@@ -16,6 +18,8 @@ function App() {
       <Route path="/Journal" element={<div>test</div>} />
       <Route path="/Community" element={<PostContent/> } />
       <Route path="/About" element={<div>Test</div> } />
+      <Route path="/Login" element={<Login/> } />
+      <Route path="/Signup" element={<Signup/> } />
     </Routes>
     {!isLoginOrSignup && <Footer />}
     </div>
