7ee9f87a3c011d83dd54dfc13052e03f8e68c474
Ahnaf Hassan <levi6516@gmail.com>
2024-02-15 01:28:05 +0600

    signup login backend

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,31 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
+const userRoutes = require('./routes/user')
 
 const app = express()
 
+// middleware
+app.use(express.json())
+
 app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
+  console.log(req.path, req.method)
+  next()
 })
 
-app.use()
+// routes
 
-app.listen(process.env.PORT, () => {
-    console.log('listening on port', process.env.PORT)
-})
+app.use('/api/user', userRoutes)
+
+// connect to db
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log('connected to db and listening on port', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
