224c10fb44e82e190b5da3fd42f875e89e30bca0
Mubtasim-Fuad-Mahde-1 <148679075+Mubtasim-Fuad-Mahde-1@users.noreply.github.com>
2024-02-19 00:51:08 +0600

    Fixed Navbar

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -4,25 +4,28 @@ import logo from '../Assets/journal_logo.png'
 import chat from '../Assets/chat.png'
 import { Link } from 'react-router-dom'
 export const Navbar = () => {
-    const[menu,setMenu] = useState("Home") 
+    const [menu, setMenu] = useState("Home")
     return (
-    <div className = 'navbar'>
-        <div className = 'navbar-logo'>
-            <img src = {logo} alt = "" />
-            <p>MoodLog</p>
-        </div>
-        <ul className = "nav-menu">
-            <li onClick={()=>{setMenu("Profile")}}><Link style={{color: '#000', textDecoration: 'none'} } to='/Profile'>Profile</Link>{menu === "Profile"? <hr/>:<></>}</li>
-            <li onClick={()=>{setMenu("Journal")}}><Link style={{color: '#000', textDecoration: 'none'} } to='/Journal'>Journal</Link>{menu === "Journal"? <hr/>:<></>}</li>
-            <li onClick={()=>{setMenu("Community")}}><Link style={{color: '#000', textDecoration: 'none'} } to='/Community'>Community</Link>{menu === "Community"? <hr/>:<></>}</li>
-            <li onClick={()=>{setMenu("About")}}><Link style={{color: '#000', textDecoration: 'none'} } to='/About'>About</Link>{menu === "About"? <hr/>:<></>}</li>
-        </ul>
-        <div className='nav-chat'>
-            <Link to='/chat'><img src={chat} alt="" /></Link>
-            <div className='nav-chat-count'> {/* this class is used to add counter to the cart icon*/}
-              10
+        <div className='navbar'>
+            <div className='navbar-logo'>
+                <img src={logo} alt="" />
+                <p>MoodLog</p>
+            </div>
+            <ul className="nav-menu">
+                <li onClick={() => { setMenu("Profile") }}><Link style={{ color: '#000', textDecoration: 'none' }} to='/Profile'>Profile</Link>{menu === "Profile" ? <hr /> : <></>}</li>
+                <li onClick={() => { setMenu("Journal") }}><Link style={{ color: '#000', textDecoration: 'none' }} to='/Journal'>Journal</Link>{menu === "Journal" ? <hr /> : <></>}</li>
+                <li onClick={() => { setMenu("Community") }}><Link style={{ color: '#000', textDecoration: 'none' }} to='/Community'>Community</Link>{menu === "Community" ? <hr /> : <></>}</li>
+                <li onClick={() => { setMenu("About") }}><Link style={{ color: '#000', textDecoration: 'none' }} to='/About'>About</Link>{menu === "About" ? <hr /> : <></>}</li>
+            </ul>
+            <div className='nav-chat'>
+                <Link to='/chat'><img src={chat} alt="" /></Link>
+                <div className='nav-chat-count'> {/* this class is used to add counter to the cart icon*/}
+                    10
+                </div>
+            </div>
+            <div className="logout">
+                <button>Logout</button>
             </div>
         </div>
-    </div>
-  )
+    )
 }
