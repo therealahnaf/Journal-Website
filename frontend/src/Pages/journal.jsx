0bd786cc36f1e684fdf1a9e8081a2d503c0289c5
Mubtasim-Fuad-Mahde-1 <148679075+Mubtasim-Fuad-Mahde-1@users.noreply.github.com>
2024-03-29 22:20:01 +0600

    completed journal frontend

--- frontend/src/Pages/journal.jsx
+++ frontend/src/Pages/journal.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import { useAuthContext } from '../Hooks/useAuthContext';
+import './Journal.css'
 //const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 export const Journal = () => {
@@ -29,7 +30,7 @@ export const Journal = () => {
       return null;
     }
   }
-  
+
   // Example usage in your component
   useEffect(() => {
     if (sentiment) {
@@ -43,7 +44,7 @@ export const Journal = () => {
         });
     }
   }, [sentiment]);
-  
+
 
 
 
@@ -65,11 +66,11 @@ export const Journal = () => {
       fetchJournals(user.email);
     }
   }, [user]);
-  
+
   useEffect(() => {
     if (journals.length > 0) {
       const journalcontentArray = journals.map(row => row.journalcontent);
-  
+
       if (journals.length > 4) {
         // If there are more than 2 journals, slice the array and join the elements
         const updatedRecentJournals = journalcontent.concat(' ').concat(journalcontentArray.slice(0, 5).join(' '));
@@ -83,7 +84,7 @@ export const Journal = () => {
       }
     }
   }, [journals]);
-  
+
   useEffect(() => {
     if (recentJournals !== '') {
       const fetchData = async () => {
@@ -92,7 +93,7 @@ export const Journal = () => {
           // Parse the JSON response
           const data = response;
           console.log(data);
-  
+
           // Find the label with the highest score
           let maxScoreLabel = '';
           let maxScore = -1;
@@ -102,11 +103,11 @@ export const Journal = () => {
               maxScoreLabel = entry.label;
             }
           }
-  
+
           // Display the label with the highest score
           setSentiment(maxScoreLabel);
           console.log("Label with highest score:", maxScoreLabel);
-  
+
           setContent('');
           // Fetch posts to update the list
           fetchJournals(user.email);
@@ -115,11 +116,11 @@ export const Journal = () => {
           console.error('Error sending message:', error);
         }
       };
-  
+
       fetchData();
     }
   }, [recentJournals]);
-  
+
 
   async function query(data) {
     const response = await fetch(
@@ -137,17 +138,17 @@ export const Journal = () => {
   const addJournalcontent = async () => {
 
     try {
-      
+
 
       const email = user.email
-			await fetch('http://localhost:4000/api/journals', {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
+      await fetch('http://localhost:4000/api/journals', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
           'Authorization': `Bearer ${user.token}`,
-				},
-				body: JSON.stringify({ journalcontent, email }),
-			});
+        },
+        body: JSON.stringify({ journalcontent, email }),
+      });
       const journalcontentArray = journals.map(row => row.journalcontent);
       console.log(journalcontentArray);
 
@@ -158,14 +159,14 @@ export const Journal = () => {
         console.log(updatedRecentJournals); // Log the updated value
       } else {
         // If there are 2 or fewer journals, join all elements
-        const updatedRecentJournals =  journalcontent.concat(' ').concat(journalcontentArray.join(' '));
+        const updatedRecentJournals = journalcontent.concat(' ').concat(journalcontentArray.join(' '));
         setrecentJournals(updatedRecentJournals);
         console.log(updatedRecentJournals); // Log the updated value
       }
-      
 
-      const response = await query({"inputs": recentJournals});
-      
+
+      const response = await query({ "inputs": recentJournals });
+
       // Parse the JSON response
       const data = response;
       console.log(data)
@@ -176,12 +177,12 @@ export const Journal = () => {
       let maxScore = -1;
       for (const entry of data[0]) {
         if (entry.score > maxScore) {
-            maxScore = entry.score;
-            maxScoreLabel = entry.label;
+          maxScore = entry.score;
+          maxScoreLabel = entry.label;
         }
 
-    }
-    
+      }
+
 
       // Display the label with the highest score
       setSentiment(maxScoreLabel)
@@ -190,54 +191,46 @@ export const Journal = () => {
       setContent('');
 
 
-        // Fetch posts to update the list
-        // fetchComments();
+      // Fetch posts to update the list
+      // fetchComments();
     } catch (error) {
-        console.error('Error sending message:', error);
+      console.error('Error sending message:', error);
     }
   }
-  
+
   return (
-    <div>
-      <div className="commentpagebox">
-          <h1>Write Journal</h1>
-          {sentiment && (
-        <div>
-          <h2>You're feeling: {sentiment}</h2>
-    
-          
+    <div className='journalfullpage'>
+      <div className="writejournal">
+        <div className="journalinput">
+          <div className="writejounraltitle">
+            <h1>Write Journal</h1>
+          </div>
+          <textarea style={{ color: '#000' }} className='journalinputarea'
+            type="text"
+            placeholder="Type todays journal"
+            value={journalcontent}
+            onChange={(e) => setContent(e.target.value)}
+          />
+          <button className='jounralsendbutton' onClick={addJournalcontent}>Send</button>
         </div>
-          )}
-        {question && (
-          <div>
-          <h2>{question}</h2>
-      
-            
-          </div>
-        
-      )}
-          <div className='commentinput'>
-              <input style={{ color: '#000' }} className='commentcontentinput'
-                  type="text"
-                  placeholder="Type your content"
-                  value={journalcontent}
-                  onChange={(e) => setContent(e.target.value)}
-              />
-              <button onClick={addJournalcontent}>Send</button>
-              
-          </div>
+        <div className="journalright">
+          <h1>{question}</h1>
+        </div>
       </div>
-      <div className='allblog'>
-				<ul className='eachblog'>
-					{journals.map((journal) => (
-						<li key={journal._id} style={{ color: '#fff' }}>
-							<p>{journal.journalcontent}</p>
-							<p className='date'>-{new Date(journal.createdAt).toLocaleString()}</p>
-						</li>
-					))}
-				</ul>
-			</div>
-  </div>
+      <div className="prevjournals">
+        <h1>Previous Journal Entries</h1>
+        <div className='prevjournalcontainer'>
+          <ul className='eachblog'>
+            {journals.map((journal) => (
+              <li key={journal._id} style={{ color: '#fff' }}>
+                <p>{journal.journalcontent}</p>
+                <p className='date'>-{new Date(journal.createdAt).toLocaleString()}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
   )
 }
 export default Journal
